32e31751f8784144cb7fcc90c721570ae16bcf25
sjcelinehong <sj.celinehong@gmail.com>
2026-05-03 01:09:58 +0000

    change

--- src/App.tsx
+++ src/App.tsx
@@ -577,7 +577,7 @@ export default function App() {
                   transition={{ duration: 0.3 }}
                 >
                   <div className="absolute inset-0 bg-black/40"></div>
-                  <div className="relative z-10 flex flex-col items-center justify-center h-full">
+                  <div className="relative z-10 w-full flex flex-col items-center justify-center h-full">
                     <h2 className="text-4xl font-serif text-white mb-4">Art</h2>
                     <img src="./asset/luca-nicoletti-nazeUct7aPs-unsplash.jpg" className="w-16 h-16 rounded" referrerPolicy="no-referrer" />
                   </div>
@@ -589,7 +589,7 @@ export default function App() {
                   transition={{ duration: 0.3 }}
                 >
                   <div className="absolute inset-0 bg-black/40"></div>
-                  <div className="relative z-10 flex flex-col items-center justify-center h-full">
+                  <div className="relative z-10 w-full flex flex-col items-center justify-center h-full">
                     <h2 className="text-4xl font-serif text-white mb-4">Novel</h2>
                     <img src="./asset/madison-o-friel-oHxUp1NdP_s-unsplash.jpg" className="w-16 h-16 rounded" referrerPolicy="no-referrer" />
                   </div>
@@ -601,7 +601,7 @@ export default function App() {
                   transition={{ duration: 0.3 }}
                 >
                   <div className="absolute inset-0 bg-black/40"></div>
-                  <div className="relative z-10 flex flex-col items-center justify-center h-full">
+                  <div className="relative z-10 w-full flex flex-col items-center justify-center h-full">
                     <h2 className="text-4xl font-serif text-white mb-4">Love</h2>
                     <img src="./asset/laura-vinck-Hyu76loQLdk-unsplash.jpg" className="w-16 h-16 rounded" referrerPolicy="no-referrer" />
                   </div>
